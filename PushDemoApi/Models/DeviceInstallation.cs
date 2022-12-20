89fc252d358a73b76f3d6269776ddeb7a180289e
saqlainawan <saqlainawanpk@outlook.com>
2022-12-20 18:24:34 +0100

    API for push notifications

---
+++ PushDemoApi/Models/DeviceInstallation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PushDemoApi.Models
+{
+    public class DeviceInstallation
+    {
+        [Required]
+        public string InstallationId { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+        [Required]
+        public string PushChannel { get; set; }
+
+        public IList<string> Tags { get; set; } = Array.Empty<string>();
+    }
+}
---
+++ PushDemoApi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+using PushDemoApi.Models;
+using PushDemoApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<INotificationService, NotificationHubService>();
+builder.Services.AddOptions<NotificationHubOptions>()
+    .Configure(builder.Configuration.GetSection("NotificationHub").Bind)
+    .ValidateDataAnnotations();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
